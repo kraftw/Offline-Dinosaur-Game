bb1bfb3ca24c5398e404dd7e81c686e3ec2c2d0b
kraftw <kraftworker12@gmail.com>
2024-02-12 23:19:54 -0500

    Finish Dino Physics

--- scenes/dino.gd
+++ scenes/dino.gd
@@ -5,8 +5,16 @@ const JUMP_SPEED : int = -1800
 
 func _physics_process(delta):
 	velocity.y += GRAVITY * delta
-	if Input.is_action_pressed("ui_accept"):
-		velocity.y = JUMP_SPEED
-		$JumpSound.play()
-		
+	if is_on_floor():
+		$RunCollision.disabled = false
+		if Input.is_action_pressed("ui_accept"):
+			velocity.y = JUMP_SPEED
+			$JumpSound.play()
+		elif Input.is_action_pressed("ui_down"):
+			$AnimatedSprite2D.play("duck")
+			$RunCollision.disabled = true
+		else:
+			$AnimatedSprite2D.play("run")
+	else:
+		$AnimatedSprite2D.play("jump")
 	move_and_slide()
